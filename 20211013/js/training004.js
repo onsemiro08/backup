e3c65389a5098c05ae1dce074f9f71a9609c56c4
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-10-13 16:01:22 +0900

    Add files via upload

---
+++ 20211013/js/training004.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.mopen').on('click',function(){
+        $('nav').toggleClass('on');
+    });
+
+    $('nav>ul>li>a').on('click', function(){
+        if($('nav').hasClass('on')){
+            $(this).next().slideToggle();
+         }
+    });
+
+    $(window).on('resize', function(){
+        $('.submenu').removeAttr('style')
+    })
+})
