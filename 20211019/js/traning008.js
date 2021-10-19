e8f4cf7e238c322f6262ce578981fdc8032b16b3
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-10-19 12:51:17 +0900

    Add files via upload

---
+++ 20211019/js/traning008.js
@@ -0,0 +1,13 @@
+$(function(){
+//------------------------------------------
+
+    $('#top_pop i').on('click',function(){
+        $('#top_pop').addClass('on');
+});
+
+$('.main_slider').slick({
+    arrows:false,
+    dots:true,
+})
+//------------------------------------------
+});
