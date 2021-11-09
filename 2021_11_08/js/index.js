60821a4a7ae3f63f41b6951efbe56d3a1a825571
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-11-09 17:49:21 +0900

    Add files via upload

---
+++ 2021_11_08/js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:true,
+        pauseOnHover:false,
+        autoplaySpeed:3000,
+        pauseOnFocus:false,
+        infinite:true,
+    });
+
+    $('#main_visual .main_btn i.xi-angle-left').on('click',function(){
+        $('.main_slider').slick('slickPrev');
+    });
+
+    $('#main_visual .main_btn i.xi-angle-right').on('click',function(){
+        $('.main_slider').slick('slickNextv');
+    });
+
+});
