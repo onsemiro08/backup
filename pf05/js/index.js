0686cd0e18e6a102b958974bcd51ea54128639ac
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-12-15 17:45:39 +0900

    Add files via upload

---
+++ pf05/js/index.js
@@ -0,0 +1,55 @@
+$(function(){
+
+    $('.top_banner i').on('click', function(){
+        $('.top_banner .case').slideToggle(500); //sliderToggle 슬라이드 위아래로
+        $(this).toggleClass('on');
+    });
+
+//$('.top_banner i').on('click', function(){
+//    $('.top_banner .case').toggleClass('on')
+//    $(this).toggleClass('on');
+    //top_banner2 .case 클릭했을 때 발생하는 이벤트 - css에서 클로즈 오픈으로 바꿈
+//    });
+
+    $('.right .menu_popup').click(function() {
+        $('.popup').fadeIn();
+            });
+
+        $('.exit').click(function() {
+        $('.popup').fadeOut();
+
+        });
+
+    var mS=$('.main_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+
+    $('.main_visual .btn li').on('click', function(){
+        var idx=$(this).index();
+        mS.slick('slickGoTo', idx)
+    });
+
+    $('.h_right_c').slick({
+        arrows:false,
+        autoplay:true,
+        slidesToShow:3,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+    });
+
+    $('.p_slider').slick({
+        arrows:false,
+        autoplay:true,
+        slidesToShow:5,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+    });
+
+});
