11d431936d5e6e7aed2115a2e2378c9515bfa65a
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-12-21 14:13:18 +0900

    Add files via upload

---
+++ pf06/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+    });
+
+
+})
