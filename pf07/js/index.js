4068e2dc30781cac19d31fd5d5f1ae51aaa44432
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2021-12-31 19:04:03 +0900

    Add files via upload

---
+++ pf07/js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $('.main_visual .popup i').on('click', function(){
+        $('.popup').toggleClass('on');
+    }) 
+
+
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+
+    $('.p_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+})
