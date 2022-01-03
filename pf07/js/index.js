2966f65daef4f3b33a95c854111d1a857cd47d2a
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2022-01-03 19:01:05 +0900

    Add files via upload

---
+++ pf07/js/index.js
@@ -0,0 +1,44 @@
+$(function(){
+
+    $('.main_visual .popup i').on('click', function(){
+        $('.popup').toggleClass('on');
+    }) 
+
+
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+
+    $('.p_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+
+    $('.tab_menu>li').on('click', function(){
+        var $this= $(this); //자기자신
+        var idx=$(this).index(); //자기자신의 번호
+        //console.log($this,idx);
+        //$('.tab_content>div').eq(idx).show().siblings.hide();
+        $('.tab_content>div').eq(idx).addClass('on').siblings().removeClass('on');
+        $this.addClass('on').siblings().removeClass('on');
+    });
+
+    $('.n_slider').slick({
+        arrows:false,
+        autoplay:false,
+        autoplaySpeed:5000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        dots:true,
+    });
+
+})
