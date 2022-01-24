b64b63eda5722b28dbf144b70b736a755384c202
onsemiro08 <91935038+onsemiro08@users.noreply.github.com>
2022-01-24 18:51:52 +0900

    Add files via upload

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -4,7 +4,7 @@ $(function(){
     let sc=$('.section');
     let sideBar=$('nav li');
     $('#main').fullpage({
-        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'Training', 'profile'],
         afterLoad:function(origin, destination, direction){
             let idx=destination.index;
             sc.eq(idx).addClass('on').siblings().removeClass('on');
@@ -29,5 +29,24 @@ $(function(){
         return false;
     })
     
+    $('.t_slider').slick({
+        arrows:false,
+        autoplay:true,
+        slidesToShow:2,
+        autoplaySpeed:4000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        focusOnSelect:true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+        ]
+
+    });
+
     //-----------------------------------------------------
 });
